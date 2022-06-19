980f1284a2628fac8271f38c537df584564b3b21
azukhrufy <anandazukhrufawalwi@gmail.com>
2022-06-20 01:37:19 +0700

    configure for responsive layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,32 +1,33 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import ProductGroup from '../modules/components/ProductGroup/ProductGroup'
 import Banner from '../common/elements/components/banner/ProductBanner'
 import RightTextBanner from '../common/elements/components/banner/RightTextBanner'
 import LeftTextBanner from '../common/elements/components/banner/LeftTextBanner'
 import {banner} from '../constant/constant'
-import ProductCard from '../common/elements/components/cards/ProductCard/ProductCard'
 
 export default function Home({data}) {
   return (
+    <>
+    <Head>
+      <title>Catalogue</title>
+      <meta name="description" content="Catalogue" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
     <div className={styles.container}>
-      <Head>
-        <title>Catalogue</title>
-        <meta name="description" content="Catalogue" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <main className={styles.main}>
         <div>
           <Banner 
             data = {banner}
           />
           <RightTextBanner />
+          <div className={styles.divider} />
           <LeftTextBanner />
           <ProductGroup data = {data} />
         </div>
       </main>
     </div>
+    </>
   )
 }
 
